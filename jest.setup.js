dbbf7d2645ea7e57817b5c2928e4f693dd6194f4
JohnVelaMacedo <jvela311297@gmail.com>
2025-02-14 15:27:00 -0500

    Add configuration

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,11 @@
 import { TextDecoder, TextEncoder } from 'util'
 import 'whatwg-fetch'
+require('dotenv').config({
+  path: '.env.test'
+})
+
+jest.mock('./src/helpers/getEnvironments', () => ({
+  getEnvironments: () => ({ ...process.env })
+}))
 
 Object.assign(global, { TextDecoder, TextEncoder })
